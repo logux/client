ff4b421438d61b28ebd309553ede55baf16c14a8
Andrey Sitnik <andrey@sitnik.ru>
2016-11-21 03:58:42 +0300

    Add Client class

---
+++ client.js
@@ -0,0 +1,140 @@
+var BrowserConnection = require('logux-sync/browser-connection')
+var createTimer = require('logux-core/create-timer')
+var ClientSync = require('logux-sync/client-sync')
+var Reconnect = require('logux-sync/reconnect')
+var shortUUID = require('short-uuid')
+var Log = require('logux-core/log')
+
+var isQuotaExceeded = require('./is-quota-exceeded')
+var LocalStore = require('./local-store')
+
+/**
+ * Low-level browser API for Logux.
+ *
+ * @param {object} options Client options.
+ * @param {string} options.url The server URL.
+ * @param {string} options.subprotocol Client subprotocol version
+ *                                     in SemVer format.
+ * @param {any} [options.credentials] Client credentials for authentication.
+ * @param {string} [options.prefix="logux"] Prefix for `localStorage` key
+ *                                          to run multiple Logux instances
+ *                                          on same web page.
+ * @param {string|number} [options.nodeId] Unique client ID.
+ *                                         Compacted UUID, by default.
+ * @param {number} [options.timeout=20000] Timeout in milliseconds
+ *                                         to disconnect connection.
+ * @param {number} [options.ping=10000] Milliseconds since last message to test
+ *                                      connection by sending ping.
+ * @param {function} [options.timer] Timer to use in log. Will be default
+ *                                   timer with server `nodeId`, by default.
+ * @param {Store} [options.store] Store to save log. Will be `LocaleStore`,
+ *                                by default.
+ * @param {number} [options.minDelay=1000] Minimum delay between reconnecting.
+ * @param {number} [options.maxDelay=5000] Maximum delay between reconnecting.
+ * @param {number} [options.attempts=Infinity] Maximum reconnecting attempts.
+ *
+ * @example
+ * token = document.querySelector('meta[name=token]')
+ *
+ * import Client from 'logux-client/client'
+ * const app = new Client({
+ *   credentials: token.content,
+ *   subprotocol: '1.0.0',
+ *   url: 'wss://example.com:1337'
+ * })
+ * app.sync.connection.connect()
+ *
+ * @class
+ */
+function Client (options) {
+  /**
+   * Client options.
+   * @type {object}
+   *
+   * @example
+   * console.log('Logux node ID is ' + app.options.nodeId)
+   */
+  this.options = options || { }
+
+  if (typeof this.options.url === 'undefined') {
+    throw new Error('Missed url option in Logux client')
+  }
+  if (typeof this.options.subprotocol === 'undefined') {
+    throw new Error('Missed subprotocol option in Logux client')
+  }
+
+  if (typeof this.options.prefix === 'undefined') {
+    this.options.prefix = 'logux'
+  }
+  if (typeof this.options.nodeId === 'undefined') {
+    var shortId = shortUUID()
+    this.options.nodeId = shortId.fromUUID(shortId.uuid())
+  }
+
+  var timer = this.options.timer || createTimer(this.options.nodeId)
+  var store = this.options.store || new LocalStore(this.options.prefix)
+
+  /**
+   * Client events log.
+   * @type {Log}
+   *
+   * @example
+   * app.log.keep(customKeeper)
+   */
+  this.log = new Log({ store: store, timer: timer })
+
+  var ws = new BrowserConnection(this.options.url)
+  var connection = new Reconnect(ws, {
+    minDelay: this.options.minDelay,
+    maxDelay: this.options.maxDelay,
+    attempts: this.options.attempts
+  })
+
+  /**
+   * Sync instance from `logux-sync` to synchronize logs.
+   * @type {ClientSync}
+   *
+   * @example
+   * if (client.sync.state === 'synchronized')
+   */
+  this.sync = new ClientSync(this.options.nodeId, this.log, connection, {
+    otherSynced: this.loadFromLS('OtherSynced'),
+    credentials: this.options.credentials,
+    subprotocol: this.options.subprotocol,
+    timeout: this.options.timeout,
+    synced: this.loadFromLS('Synced'),
+    ping: this.options.ping
+  })
+
+  var app = this
+  this.unbind = []
+
+  this.unbind.push(this.sync.on('synced', function () {
+    app.saveToLS('Synced', app.sync.synced)
+    app.saveToLS('OtherSynced', app.sync.otherSynced)
+  }))
+}
+
+Client.prototype = {
+
+  loadFromLS: function loadFromLS (field) {
+    if (global.localStorage) {
+      var value = parseInt(localStorage.getItem(this.options.prefix + field))
+      return isNaN(value) ? undefined : value
+    } else {
+      return undefined
+    }
+  },
+
+  saveToLS: function saveToLS (field, value) {
+    if (global.localStorage) {
+      try {
+        localStorage.setItem(this.options.prefix + field, value)
+      } catch (e) {
+        if (!isQuotaExceeded(e)) throw e
+      }
+    }
+  }
+}
+
+module.exports = Client
---
+++ test/client.test.js
@@ -0,0 +1,176 @@
+var createTestTimer = require('logux-core').createTestTimer
+var MemoryStore = require('logux-core').MemoryStore
+require('mock-local-storage')
+
+var Client = require('../client')
+
+var originStorage = localStorage
+afterEach(function () {
+  global.localStorage = originStorage
+  localStorage.clear()
+  localStorage.itemInsertionCallback = null
+})
+
+it('saves options', function () {
+  var client = new Client({ subprotocol: '1.0.0', url: 'wss://localhost:1337' })
+  expect(client.options.subprotocol).toEqual('1.0.0')
+})
+
+it('throws on missed URL', function () {
+  expect(function () {
+    new Client({ subprotocol: '1.0.0' })
+  }).toThrowError(/url/)
+})
+
+it('throws on missed subprotocol', function () {
+  expect(function () {
+    new Client({ url: 'wss://localhost:1337' })
+  }).toThrowError(/subprotocol/)
+})
+
+it('generates node ID if it is missed', function () {
+  var client = new Client({ subprotocol: '1.0.0', url: 'wss://localhost:1337' })
+  expect(client.options.nodeId).toMatch(/\d\w+/)
+})
+
+it('uses custom node ID', function () {
+  var client = new Client({
+    subprotocol: '1.0.0',
+    nodeId: 'client',
+    url: 'wss://localhost:1337'
+  })
+  expect(client.options.nodeId).toEqual('client')
+})
+
+it('uses node ID in timer', function () {
+  var client = new Client({ subprotocol: '1.0.0', url: 'wss://localhost:1337' })
+  var time = client.log.timer()
+  expect(typeof time[0]).toEqual('number')
+  expect(time[1]).toEqual(client.options.nodeId)
+  expect(time[2]).toBe(0)
+})
+
+it('uses custom timer', function () {
+  var client = new Client({
+    subprotocol: '1.0.0',
+    timer: createTestTimer(),
+    url: 'wss://localhost:1337'
+  })
+  expect(client.log.timer()).toEqual([1])
+})
+
+it('uses custom store', function () {
+  var store = new MemoryStore()
+  var client = new Client({
+    subprotocol: '1.0.0',
+    store: store,
+    url: 'wss://localhost:1337'
+  })
+  expect(client.log.store).toBe(store)
+})
+
+it('uses default synced', function () {
+  var client = new Client({ subprotocol: '1.0.0', url: 'wss://localhost:1337' })
+  expect(client.sync.synced).toBe(0)
+  expect(client.sync.otherSynced).toBe(0)
+})
+
+it('stores synced from LocalStorage', function () {
+  localStorage.setItem('loguxSynced', 100)
+  localStorage.setItem('loguxOtherSynced', 200)
+
+  var client = new Client({ subprotocol: '1.0.0', url: 'wss://localhost:1337' })
+  expect(client.log.store.key).toEqual('loguxLog')
+  expect(client.sync.synced).toBe(100)
+  expect(client.sync.otherSynced).toBe(200)
+
+  client.sync.setSynced(101)
+  expect(localStorage.getItem('loguxSynced')).toEqual('101')
+
+  client.sync.setOtherSynced(201)
+  expect(localStorage.getItem('loguxOtherSynced')).toEqual('201')
+})
+
+it('uses custom prefix', function () {
+  localStorage.setItem('appSynced', 1)
+  localStorage.setItem('appOtherSynced', 2)
+  var client = new Client({
+    subprotocol: '1.0.0',
+    prefix: 'app',
+    url: 'wss://localhost:1337'
+  })
+
+  expect(client.log.store.key).toEqual('appLog')
+  expect(client.sync.synced).toBe(1)
+  expect(client.sync.otherSynced).toBe(2)
+})
+
+it('works without localStorage', function () {
+  global.localStorage = undefined
+  var client = new Client({
+    subprotocol: '1.0.0',
+    store: new MemoryStore(),
+    url: 'wss://localhost:1337'
+  })
+  expect(client.sync.synced).toBe(0)
+})
+
+it('works on localStorage limit error', function () {
+  localStorage.itemInsertionCallback = function () {
+    var err = new Error('Mock localStorage quota exceeded')
+    err.code = DOMException.QUOTA_EXCEEDED_ERR
+    throw err
+  }
+
+  var client = new Client({
+    subprotocol: '1.0.0',
+    store: new MemoryStore(),
+    url: 'wss://localhost:1337'
+  })
+  client.sync.setSynced(1)
+})
+
+it('throws other errors from localStorage', function () {
+  localStorage.itemInsertionCallback = function () {
+    throw new Error('Test')
+  }
+  var client = new Client({
+    subprotocol: '1.0.0',
+    store: new MemoryStore(),
+    url: 'wss://localhost:1337'
+  })
+
+  expect(function () {
+    client.sync.setSynced(1)
+  }).toThrowError('Test')
+})
+
+it('sends options to connection', function () {
+  var client = new Client({
+    subprotocol: '1.0.0',
+    minDelay: 100,
+    maxDelay: 500,
+    attempts: 5,
+    url: 'wss://localhost:1337'
+  })
+  expect(client.sync.connection.options).toEqual({
+    minDelay: 100,
+    maxDelay: 500,
+    attempts: 5
+  })
+  expect(client.sync.connection.connection.url).toEqual('wss://localhost:1337')
+})
+
+it('sends options to sync', function () {
+  var client = new Client({
+    subprotocol: '1.0.0',
+    credentials: 'token',
+    timeout: 2000,
+    ping: 1000,
+    url: 'wss://localhost:1337'
+  })
+  expect(client.sync.options.subprotocol).toEqual('1.0.0')
+  expect(client.sync.options.credentials).toEqual('token')
+  expect(client.sync.options.timeout).toEqual(2000)
+  expect(client.sync.options.ping).toEqual(1000)
+})
