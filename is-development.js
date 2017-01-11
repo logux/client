09028d6d964db76d6d0b7d53746615f318a98896
Dmitriy Semyushkin <git@devg.ru>
2017-01-11 15:55:07 +0300

    Use proper regex for 127.* localhost detection

--- is-development.js
+++ is-development.js
@@ -1,7 +1,7 @@
 module.exports = function isDevelopment (url) {
   return (
     /^wss?:\/\/localhost/.test(url) ||
-    /^wss?:\/\/127\.0\.0\.1/.test(url) ||
+    /^wss?:\/\/127\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/.test(url) ||
     /^wss?:\/\/ip6-localhost/.test(url) ||
     /^wss?:\/\/::1/.test(url) ||
     /^wss?:\/\/[^/\s:]+\.dev(\/|:|$)/.test(url)
