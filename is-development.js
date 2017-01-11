58d52abc88d68b29572aa39ce1f2c4cd5aa5ac43
Andrey Sitnik <andrey@sitnik.ru>
2017-01-12 03:00:37 +1000

    Remove rare local IP mask

--- is-development.js
+++ is-development.js
@@ -5,7 +5,6 @@ module.exports = function isDevelopment (url) {
     /^wss?:\/\/ip6-localhost/.test(url) ||
     /^wss?:\/\/::1/.test(url) ||
     /^wss?:\/\/[^/\s:]+\.dev(\/|:|$)/.test(url) ||
-    /^wss?:\/\/[^/\s:]+\.local(\/|:|$)/.test(url) ||
-    /^wss?:\/\/169\.254\.\d+\.\d+/.test(url)
+    /^wss?:\/\/[^/\s:]+\.local(\/|:|$)/.test(url)
   )
 }
