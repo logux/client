a867715a53b9c36c7392e36201f6b408a71ea76d
Andrey Sitnik <andrey@sitnik.ru>
2017-01-31 20:18:26 +0530

    Clean up store for IndexedDB

---
+++ indexed-store.js
@@ -0,0 +1,191 @@
+var VERSION = 1
+
+function promisify (request) {
+  return new Promise(function (resolve, reject) {
+    request.onsuccess = function (e) {
+      resolve(e.target.result)
+    }
+    request.onerror = function (e) {
+      reject(e.target.error)
+    }
+  })
+}
+
+function nextEntry (request) {
+  return function (cursor) {
+    if (cursor) {
+      cursor.value.meta.added = cursor.value.added
+      return {
+        entries: [[cursor.value.action, cursor.value.meta]],
+        next: function () {
+          cursor.continue()
+          return promisify(request).then(nextEntry(request))
+        }
+      }
+    } else {
+      return { entries: [] }
+    }
+  }
+}
+
+/**
+ * `IndexedDB` store for Logux log.
+ *
+ * @param {string} [name="logux"] Database name to run multiple
+ *                                Logux instances on same web page.
+ *
+ * @class
+ * @extends Store
+ *
+ * @example
+ * import IndexedStore from 'logux-client/indexed-store'
+ * var log = new Log({ store: new IndexedStore(), nodeId })
+ */
+function IndexedStore (name) {
+  this.name = name || 'logux'
+}
+
+IndexedStore.prototype = {
+
+  init: function init () {
+    if (this.initing) return this.initing
+
+    var store = this
+    var opening = indexedDB.open(this.name, VERSION)
+
+    opening.onupgradeneeded = function (e) {
+      var db = e.target.result
+
+      var log = db.createObjectStore('log', {
+        keyPath: 'added',
+        autoIncrement: true
+      })
+      log.createIndex('id', 'id', { unique: true })
+      log.createIndex('created', 'created', { unique: true })
+
+      db.createObjectStore('extra', { keyPath: 'key' })
+        .transaction.oncomplete = function () {
+          db.transaction('extra', 'readwrite')
+            .objectStore('extra')
+            .add({ key: 'lastSynced', sent: 0, received: 0 })
+        }
+    }
+
+    this.initing = promisify(opening).then(function (db) {
+      store.db = db
+      db.onversionchange = function () {
+        store.db.close()
+        if (global.document && global.document.reload) {
+          global.document.reload()
+        }
+      }
+      return store
+    })
+
+    return this.initing
+  },
+
+  get: function get (order) {
+    var request
+    return this.init().then(function (store) {
+      var log = store.os('log')
+      if (order === 'created') {
+        request = log.index('created').openCursor(null, 'prev')
+      } else {
+        request = log.openCursor(null, 'prev')
+      }
+      return promisify(request).then(nextEntry(request))
+    })
+  },
+
+  add: function add (action, meta) {
+    var entry = {
+      id: meta.id,
+      meta: meta,
+      time: meta.time,
+      action: action,
+      created: meta.time + '\t' + meta.id.slice(1).join('\t')
+    }
+
+    return this.init().then(function (store) {
+      var log = store.os('log', 'write')
+      return promisify(log.index('id').get(meta.id)).then(function (exist) {
+        if (exist) {
+          return false
+        } else {
+          return promisify(log.add(entry)).then(function (added) {
+            meta.added = added
+            return meta
+          })
+        }
+      })
+    })
+  },
+
+  remove: function remove (id) {
+    return this.init().then(function (store) {
+      var log = store.os('log', 'write')
+      return promisify(log.index('id').get(id)).then(function (entry) {
+        if (!entry) {
+          return false
+        } else {
+          return promisify(log.delete(entry.added))
+        }
+      })
+    })
+  },
+
+  getLastAdded: function getLastAdded () {
+    return this.init().then(function (store) {
+      return promisify(store.os('log').openCursor(null, 'prev'))
+    }).then(function (cursor) {
+      return cursor ? cursor.value.added : 0
+    })
+  },
+
+  getLastSynced: function getLastSynced () {
+    return this.init().then(function (store) {
+      return promisify(store.os('extra').get('lastSynced'))
+    }).then(function (data) {
+      return { sent: data.sent, received: data.received }
+    })
+  },
+
+  setLastSynced: function setLastSynced (values) {
+    return this.init().then(function (store) {
+      var extra = store.os('extra', 'write')
+      return promisify(extra.get('lastSynced')).then(function (data) {
+        if (typeof values.sent !== 'undefined') {
+          data.sent = values.sent
+        }
+        if (typeof values.received !== 'undefined') {
+          data.received = values.received
+        }
+        return promisify(extra.put(data))
+      })
+    })
+  },
+
+  os: function os (name, write) {
+    var mode = write ? 'readwrite' : 'readonly'
+    return this.db.transaction(name, mode).objectStore(name)
+  },
+
+  /**
+   * Remove all database and data from `indexedDB`.
+   *
+   * @return {Promise} Promise for end of removing
+   *
+   * @example
+   * afterEach(() => this.store.destroy())
+   */
+  destroy: function destroy () {
+    return this.init().then(function (store) {
+      store.db.close()
+      return promisify(global.indexedDB.deleteDatabase(store.name))
+    })
+  }
+
+}
+
+module.exports = IndexedStore
---
+++ test/indexed-store.test.js
@@ -0,0 +1,220 @@
+/* eslint-disable no-invalid-this */
+
+var fakeIndexedDB = require('fake-indexeddb')
+
+var IndexedStore = require('../indexed-store')
+
+var lastStore = 0
+function createStore () {
+  lastStore += 1
+  return new IndexedStore(lastStore)
+}
+
+var originIndexedDB = global.indexedDB
+beforeEach(function () {
+  global.indexedDB = fakeIndexedDB
+})
+
+afterEach(function () {
+  return Promise.all([
+    this.store ? this.store.destroy() : null,
+    this.other ? this.other.destroy() : null
+  ]).then(function () {
+    global.indexedDB = originIndexedDB
+    delete global.document.reload
+  })
+})
+
+function all (request, list) {
+  if (!list) list = []
+  return request.then(function (page) {
+    list = list.concat(page.entries)
+    if (page.next) {
+      return all(page.next(), list)
+    } else {
+      return list
+    }
+  })
+}
+
+function check (store, created, added) {
+  if (!added) added = created
+  return all(store.get('created')).then(function (entries) {
+    expect(entries).toEqual(created)
+  }).then(function () {
+    return all(store.get('added'))
+  }).then(function (entries) {
+    expect(entries).toEqual(added)
+  })
+}
+
+it('allows to change DB name', function () {
+  this.store = new IndexedStore('custom')
+  return this.store.init().then(function (store) {
+    expect(store.db.name).toEqual('custom')
+    expect(store.name).toEqual('custom')
+  })
+})
+
+it('is empty in the beginning', function () {
+  this.store = createStore()
+  var test = this
+  return check(this.store, []).then(function () {
+    return test.store.getLastAdded()
+  }).then(function (added) {
+    expect(added).toEqual(0)
+    return test.store.getLastSynced()
+  }).then(function (synced) {
+    expect(synced).toEqual({ sent: 0, received: 0 })
+  })
+})
+
+it('updates last sent value', function () {
+  this.store = createStore()
+  var test = this
+  return this.store.setLastSynced({ sent: 1 }).then(function () {
+    return test.store.getLastSynced()
+  }).then(function (synced) {
+    return expect(synced).toEqual({ sent: 1, received: 0 })
+  }).then(function () {
+    return test.store.setLastSynced({ sent: 2, received: 1 })
+  }).then(function () {
+    return test.store.getLastSynced()
+  }).then(function (synced) {
+    return expect(synced).toEqual({ sent: 2, received: 1 })
+  }).then(function () {
+    test.other = new IndexedStore(test.store.name)
+    return test.other.getLastSynced()
+  }).then(function (synced) {
+    return expect(synced).toEqual({ sent: 2, received: 1 })
+  })
+})
+
+it('stores entries sorted', function () {
+  this.store = createStore()
+  var test = this
+  return Promise.all([
+    this.store.add({ type: '1' }, { id: [1, 'a'], time: 1 }),
+    this.store.add({ type: '2' }, { id: [1, 'c'], time: 2 }),
+    this.store.add({ type: '3' }, { id: [1, 'b'], time: 2 })
+  ]).then(function () {
+    return check(test.store, [
+      [{ type: '2' }, { added: 2, id: [1, 'c'], time: 2 }],
+      [{ type: '3' }, { added: 3, id: [1, 'b'], time: 2 }],
+      [{ type: '1' }, { added: 1, id: [1, 'a'], time: 1 }]
+    ], [
+      [{ type: '3' }, { added: 3, id: [1, 'b'], time: 2 }],
+      [{ type: '2' }, { added: 2, id: [1, 'c'], time: 2 }],
+      [{ type: '1' }, { added: 1, id: [1, 'a'], time: 1 }]
+    ])
+  })
+})
+
+it('stores any metadata', function () {
+  this.store = createStore()
+  var test = this
+  return this.store.add(
+    { type: 'a' },
+    { id: [1, 'a'], time: 1, test: 1 }
+  ).then(function () {
+    return check(test.store, [
+      [{ type: 'a' }, { added: 1, id: [1, 'a'], time: 1, test: 1 }]
+    ])
+  })
+})
+
+it('ignores entries with same ID', function () {
+  this.store = createStore()
+  var test = this
+  var id = [1, 'a', 1]
+  return this.store.add({ a: 1 }, { id: id, time: 1 }).then(function (meta) {
+    expect(meta).toEqual({ id: id, time: 1, added: 1 })
+    return test.store.add({ a: 2 }, { id: id, time: 2 })
+  }).then(function (meta) {
+    expect(meta).toBeFalsy()
+    return check(test.store, [
+      [{ a: 1 }, { id: id, time: 1, added: 1 }]
+    ])
+  })
+})
+
+it('removes entries', function () {
+  this.store = createStore()
+  var test = this
+  return Promise.all([
+    this.store.add({ type: '1' }, { id: [1], time: 1 }),
+    this.store.add({ type: '2' }, { id: [2], time: 2 }),
+    this.store.add({ type: '3' }, { id: [3], time: 3 })
+  ]).then(function () {
+    return test.store.remove([2])
+  }).then(function () {
+    return check(test.store, [
+      [{ type: '3' }, { id: [3], time: 3, added: 3 }],
+      [{ type: '1' }, { id: [1], time: 1, added: 1 }]
+    ])
+  })
+})
+
+it('ignores unknown entry', function () {
+  this.store = createStore()
+  return this.store.remove([2]).then(function (removed) {
+    expect(removed).toBeFalsy()
+  })
+})
+
+it('returns last added', function () {
+  this.store = createStore()
+  var test = this
+  return this.store.add({ type: 'a' }, { id: [1], time: 1 }).then(function () {
+    return test.store.add({ type: 'b' }, { id: [2], time: 2 })
+  }).then(function () {
+    return test.store.getLastAdded()
+  }).then(function (added) {
+    return expect(added).toBe(2)
+  }).then(function () {
+    test.other = new IndexedStore(test.store.name)
+    return test.other.getLastAdded()
+  }).then(function (added) {
+    return expect(added).toBe(2)
+  })
+})
+
+it('reloads page on database update', function () {
+  document.reload = jest.fn()
+  this.store = createStore()
+  var test = this
+  return this.store.init().then(function () {
+    var opening = indexedDB.open(test.store.name, 1000)
+    return new Promise(function (resolve, reject) {
+      opening.onsuccess = function (e) {
+        e.target.result.close()
+        resolve()
+      }
+      opening.onerror = function (e) {
+        reject(e.target.error)
+      }
+    })
+  }).then(function () {
+    expect(document.reload).toHaveBeenCalled()
+  })
+})
+
+it('throws a errors', function () {
+  var error = new Error()
+  global.indexedDB = {
+    open: function () {
+      var request = { }
+      setTimeout(function () {
+        request.onerror({ target: { error: error } })
+      }, 1)
+      return request
+    }
+  }
+  var store = new IndexedStore()
+  var throwed
+  return store.init().catch(function (e) {
+    throwed = e
+  }).then(function () {
+    expect(throwed).toBe(error)
+  })
+})
