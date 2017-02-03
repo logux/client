8108b8a806be0c9c3ddfb4ea8eb4660a520b1089
Andrey Sitnik <andrey@sitnik.ru>
2017-02-03 19:24:47 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var Analyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+var webpack = require('webpack')
+
+module.exports = {
+  module: {
+    loaders: [
+      { test: /\.json$/, loader: 'json-loader' }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({ minimize: true }),
+    new Analyzer({
+      analyzerMode: 'static',
+      reportFilename: 'bundle-report.html',
+      openAnalyzer: false
+    })
+  ]
+}
