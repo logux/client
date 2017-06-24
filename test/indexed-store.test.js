6c4986cfe59592ac6280aebe9db6ca38a4ab842e
Konstantin Mamaev <konstantin.mamaev@akvelon.com>
2017-06-24 15:25:27 +0300

    Remove common tests

--- test/indexed-store.test.js
+++ test/indexed-store.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-invalid-this */
 
 var fakeIndexedDB = require('fake-indexeddb')
+var eachTest = require('logux-store-tests')
 var TestTime = require('logux-core').TestTime
 
 var IndexedStore = require('../indexed-store')
@@ -49,6 +50,13 @@ function check (indexed, created, added) {
 
 function nope () { }
 
+eachTest(function (desc, creator) {
+  it(desc, creator(function () {
+    store = new IndexedStore()
+    return store
+  }))
+})
+
 it('use logux as default name', function () {
   store = new IndexedStore()
   return store.init().then(function () {
@@ -62,57 +70,6 @@ it('allows to change DB name', function () {
   return store.init().then(function () {
     expect(store.db.name).toEqual('custom')
     expect(store.name).toEqual('custom')
-  })
-})
-
-it('is empty in the beginning', function () {
-  store = new IndexedStore()
-  return check(store, []).then(function () {
-    return store.getLastAdded()
-  }).then(function (added) {
-    expect(added).toEqual(0)
-    return store.getLastSynced()
-  }).then(function (synced) {
-    expect(synced).toEqual({ sent: 0, received: 0 })
-  })
-})
-
-it('updates last sent value', function () {
-  store = new IndexedStore()
-  return store.setLastSynced({ sent: 1 }).then(function () {
-    return store.getLastSynced()
-  }).then(function (synced) {
-    return expect(synced).toEqual({ sent: 1, received: 0 })
-  }).then(function () {
-    return store.setLastSynced({ sent: 2, received: 1 })
-  }).then(function () {
-    return store.getLastSynced()
-  }).then(function (synced) {
-    return expect(synced).toEqual({ sent: 2, received: 1 })
-  }).then(function () {
-    other = new IndexedStore(store.name)
-    return other.getLastSynced()
-  }).then(function (synced) {
-    return expect(synced).toEqual({ sent: 2, received: 1 })
-  })
-})
-
-it('stores entries sorted', function () {
-  store = new IndexedStore()
-  return Promise.all([
-    store.add({ type: '1' }, { id: [1, 'a'], time: 1 }),
-    store.add({ type: '2' }, { id: [1, 'c'], time: 2 }),
-    store.add({ type: '3' }, { id: [1, 'b'], time: 2 })
-  ]).then(function () {
-    return check(store, [
-      [{ type: '2' }, { added: 2, id: [1, 'c'], time: 2 }],
-      [{ type: '3' }, { added: 3, id: [1, 'b'], time: 2 }],
-      [{ type: '1' }, { added: 1, id: [1, 'a'], time: 1 }]
-    ], [
-      [{ type: '3' }, { added: 3, id: [1, 'b'], time: 2 }],
-      [{ type: '2' }, { added: 2, id: [1, 'c'], time: 2 }],
-      [{ type: '1' }, { added: 1, id: [1, 'a'], time: 1 }]
-    ])
   })
 })
 
@@ -139,22 +96,6 @@ it('ignores entries with same ID', function () {
     return check(store, [
       [{ a: 1 }, { id: id, time: 1, added: 1 }]
     ])
-  })
-})
-
-it('returns last added', function () {
-  store = new IndexedStore()
-  return store.add({ type: 'A' }, { id: [1], time: 1 }).then(function () {
-    return store.add({ type: 'B' }, { id: [2], time: 2 })
-  }).then(function () {
-    return store.getLastAdded()
-  }).then(function (added) {
-    return expect(added).toBe(2)
-  }).then(function () {
-    other = new IndexedStore(store.name)
-    return other.getLastAdded()
-  }).then(function (added) {
-    return expect(added).toBe(2)
   })
 })
 
@@ -189,25 +130,6 @@ it('checks that action ID is used in log', function () {
   })
 })
 
-it('changes meta', function () {
-  store = new IndexedStore()
-  return store.add({ }, { id: [1], time: 1, a: 1 }).then(function () {
-    return store.changeMeta([1], { a: 2, b: 2 })
-  }).then(function (result) {
-    expect(result).toBeTruthy()
-    return check(store, [
-      [{ }, { id: [1], time: 1, added: 1, a: 2, b: 2 }]
-    ])
-  })
-})
-
-it('resolves to false on unknown ID in changeMeta', function () {
-  store = new IndexedStore()
-  return store.changeMeta([1], { a: 1 }).then(function (result) {
-    expect(result).toBeFalsy()
-  })
-})
-
 it('works with real log', function () {
   store = new IndexedStore()
   var log = TestTime.getLog({ store: store })
@@ -224,37 +146,11 @@ it('works with real log', function () {
   })
 })
 
-it('removes entries', function () {
-  store = new IndexedStore()
-  return Promise.all([
-    store.add({ type: '1' }, { id: [1, 'node', 0], time: 1 }),
-    store.add({ type: '2' }, { id: [2, 'node', 0], time: 2 }),
-    store.add({ type: '3' }, { id: [3, 'node', 0], time: 3 })
-  ]).then(function () {
-    return store.remove([2, 'node', 0])
-  }).then(function (entry) {
-    expect(entry).toEqual([
-      { type: '2' }, { id: [2, 'node', 0], time: 2, added: 2 }
-    ])
-    return check(store, [
-      [{ type: '3' }, { id: [3, 'node', 0], time: 3, added: 3 }],
-      [{ type: '1' }, { id: [1, 'node', 0], time: 1, added: 1 }]
-    ])
-  })
-})
-
-it('ignores unknown entry', function () {
-  store = new IndexedStore()
-  return store.remove([2]).then(function (removed) {
-    expect(removed).toBeFalsy()
-  })
-})
-
 it('throws a errors', function () {
   var error = new Error()
   global.indexedDB = {
     open: function () {
-      var request = { }
+      var request = {}
       setTimeout(function () {
         request.onerror({ target: { error: error } })
       }, 1)
@@ -270,96 +166,15 @@ it('throws a errors', function () {
   })
 })
 
-it('removes reasons and actions without reason', function () {
-  store = new IndexedStore()
-  var removed = []
-  return Promise.all([
-    store.add({ type: '1' }, { id: [1], time: 1, reasons: ['a'] }),
-    store.add({ type: '2' }, { id: [2], time: 2, reasons: ['a'] }),
-    store.add({ type: '3' }, { id: [3], time: 3, reasons: ['a', 'b'] }),
-    store.add({ type: '4' }, { id: [4], time: 4, reasons: ['b'] })
-  ]).then(function () {
-    return store.removeReason('a', { }, function (action, meta) {
-      removed.push([action, meta])
-    })
-  }).then(function () {
-    expect(removed).toEqual([
-      [{ type: '1' }, { added: 1, id: [1], time: 1, reasons: [] }],
-      [{ type: '2' }, { added: 2, id: [2], time: 2, reasons: [] }]
-    ])
-    return check(store, [
-      [{ type: '4' }, { added: 4, id: [4], time: 4, reasons: ['b'] }],
-      [{ type: '3' }, { added: 3, id: [3], time: 3, reasons: ['b'] }]
-    ])
-  })
-})
-
-it('removes reason with minimum added', function () {
-  store = new IndexedStore()
-  return Promise.all([
-    store.add({ type: '1' }, { id: [1], time: 1, reasons: ['a'] }),
-    store.add({ type: '2' }, { id: [2], time: 2, reasons: ['a'] }),
-    store.add({ type: '3' }, { id: [3], time: 3, reasons: ['a'] })
-  ]).then(function () {
-    return store.removeReason('a', { minAdded: 2 }, nope)
-  }).then(function () {
-    return check(store, [
-      [{ type: '1' }, { added: 1, id: [1], time: 1, reasons: ['a'] }]
-    ])
-  })
-})
-
-it('removes reason with maximum added', function () {
-  store = new IndexedStore()
-  return Promise.all([
-    store.add({ type: '1' }, { id: [1], time: 1, reasons: ['a'] }),
-    store.add({ type: '2' }, { id: [2], time: 2, reasons: ['a'] }),
-    store.add({ type: '3' }, { id: [3], time: 3, reasons: ['a'] })
-  ]).then(function () {
-    return store.removeReason('a', { maxAdded: 2 }, nope)
-  }).then(function () {
-    return check(store, [
-      [{ type: '3' }, { added: 3, id: [3], time: 3, reasons: ['a'] }]
-    ])
-  })
-})
-
-it('removes reason with minimum and maximum added', function () {
-  store = new IndexedStore()
-  return Promise.all([
-    store.add({ type: '1' }, { id: [1], time: 1, reasons: ['a'] }),
-    store.add({ type: '2' }, { id: [2], time: 2, reasons: ['a'] }),
-    store.add({ type: '3' }, { id: [3], time: 3, reasons: ['a'] })
-  ]).then(function () {
-    return store.removeReason('a', { maxAdded: 2, minAdded: 2 }, nope)
-  }).then(function () {
-    return check(store, [
-      [{ type: '3' }, { added: 3, id: [3], time: 3, reasons: ['a'] }],
-      [{ type: '1' }, { added: 1, id: [1], time: 1, reasons: ['a'] }]
-    ])
-  })
-})
-
-it('removes reason with zero at maximum added', function () {
-  store = new IndexedStore()
-  return store.add({ }, { id: [1], time: 1, reasons: ['a'] }).then(function () {
-    return store.removeReason('a', { maxAdded: 0 }, nope)
-  }).then(function () {
-    return check(store, [
-      [{ }, { added: 1, id: [1], time: 1, reasons: ['a'] }]
-    ])
-  })
-})
-
 it('updates reasons cache', function () {
   store = new IndexedStore()
-  return store.add({ }, { id: [1], time: 1, reasons: ['a'] }).then(function () {
+  return store.add({}, { id: [1], time: 1, reasons: ['a'] }).then(function () {
     return store.changeMeta([1], { reasons: ['a', 'b', 'c'] })
   }).then(function () {
-    return store.removeReason('b', { }, nope)
+    return store.removeReason('b', {}, nope)
   }).then(function () {
     return check(store, [
-      [{ }, { added: 1, id: [1], time: 1, reasons: ['a', 'c'] }]
+      [{}, { added: 1, id: [1], time: 1, reasons: ['a', 'c'] }]
     ])
   })
 })
