4c1ffdcfc4a9bef41cecb699784c96f945797337
Andrey Sitnik <andrey@sitnik.ru>
2017-06-24 22:04:23 -0300

    Normalize dot in badge translations

--- badge/en.js
+++ badge/en.js
@@ -1,9 +1,9 @@
 module.exports = {
   synchronized: 'Your data has been saved',
   disconnected: 'No Internet connection',
-  wait: 'No Internet connection.<br>Your data has not been saved.',
+  wait: 'No Internet connection<br>Your data has not been saved',
   connecting: 'Data saving',
   sending: 'Data saving',
-  error: 'Server error.<br>Your data has not been saved',
-  protocolError: 'Saving is not working.<br>Refresh the page'
+  error: 'Server error<br>Your data has not been saved',
+  protocolError: 'Saving is not working<br>Refresh the page'
 }
