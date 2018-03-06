a5278723a333191d3f6059147a3776b222673c1f
Andrey Sitnik <andrey@sitnik.ru>
2018-03-06 12:33:17 -0500

    Speed up demo server

--- test/demo/webpack.config.js
+++ test/demo/webpack.config.js
@@ -2,6 +2,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: path.join(__dirname, 'index.js'),
   output: {
     path: path.join(__dirname, 'build'),
