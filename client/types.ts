0c33c28634b3b219265e6fc4b9e2644c85cff689
Andrey Sitnik <andrey@sitnik.ru>
2020-03-28 22:08:15 -0400

    Improve types

--- client/types.ts
+++ client/types.ts
@@ -1,3 +1,4 @@
+import { Action } from '@logux/core'
 import { Client } from '..'
 
 let app = new Client({
@@ -7,3 +8,19 @@ let app = new Client({
 })
 
 app.log.add({ type: 'A' }, { extra: 1 })
+
+type UserRenameAction = Action & {
+  type: 'user/rename',
+  userId: string,
+  name: string
+}
+
+function isUserRename (action): action is UserRenameAction {
+  return action.type === 'user/rename'
+}
+
+app.log.on('add', action => {
+  if (isUserRename(action)) {
+    document.title = action.name
+  }
+})
