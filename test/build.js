29716beaef8faeb3a72f9fddb9a9451da8d03f70
Andrey Sitnik <andrey@sitnik.ru>
2020-10-15 20:48:02 -0400

    Add Node.js 10 and 12 to Travis CI to test SSR

---
+++ test/build.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+let { join } = require('path')
+let Bundler = require('parcel-bundler')
+let ciJobNumber = require('ci-job-number')
+
+if (ciJobNumber() > 1) {
+  process.stdout.write('Avoid building demo website to keep CI resources')
+  process.exit(0)
+}
+
+async function build () {
+  let bundler = new Bundler(join(__dirname, '../test/demo/index.html'), {
+    outDir: join(__dirname, '../test/demo/build'),
+    cacheDir: join(__dirname, '../test/demo/.cache'),
+    sourceMaps: false,
+    scopeHoist: true
+  })
+  await bundler.bundle()
+}
+
+build().catch(e => {
+  process.stderr.write(e.stack + '\n')
+  process.exit(1)
+})
