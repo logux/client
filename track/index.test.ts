884d1ce2faf4a6fbce9167cf9b78e553b0489da8
Andrey Sitnik <andrey@sitnik.ru>
2020-11-10 18:45:42 -0500

    Fix multiple track()

--- track/index.test.ts
+++ track/index.test.ts
@@ -39,3 +39,31 @@ it('tracks action processing', async () => {
   await delay(10)
   expect(results).toEqual(['Server undid Logux action because of test'])
 })
+
+it('works on multiple calls', async () => {
+  let pair = new TestPair()
+  let client = new Client<{}, TestLog>({
+    subprotocol: '1.0.0',
+    userId: '10',
+    server: pair.left,
+    time: new TestTime()
+  })
+  client.node.catch(() => {})
+
+  let resolves: string[] = []
+
+  let id = client.log.generateId()
+  client.sync({ type: 'FOO' }, { id }).then(() => {
+    resolves.push('sync')
+  })
+  track(client, id).then(() => {
+    resolves.push('track1')
+  })
+  track(client, id).then(() => {
+    resolves.push('track2')
+  })
+
+  await client.log.add({ type: 'logux/processed', id })
+  await delay(10)
+  expect(resolves).toEqual(['sync', 'track1', 'track2'])
+})
