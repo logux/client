72f6b6f9fcff5e3d8f93bc198a6f80b81f357e45
Andrey Sitnik <andrey@sitnik.ru>
2020-11-17 21:20:49 -0500

    Clean up code

--- client/index.test.ts
+++ client/index.test.ts
@@ -684,12 +684,12 @@ it('changes user ID of disconnected node', async () => {
   let client = await createDialog()
   client.node.connection.disconnect()
   client.changeUser('20', 'token')
-  let clientID = client.clientId.split(':')[1]
-  expect(client.node.localNodeId).toEqual(`20:${clientID}:1`)
+  let clientId = client.clientId.split(':')[1]
+  expect(client.node.localNodeId).toEqual(`20:${clientId}:1`)
   expect(client.node.state).toEqual('disconnected')
   let meta = await client.log.add({ type: 'test' })
   if (meta === false) throw new Error('Action was not found')
-  expect(meta.id).toContain(` 20:${clientID}:1 `)
+  expect(meta.id).toContain(` 20:${clientId}:1 `)
 })
 
 it('checks user ID during changing', async () => {
