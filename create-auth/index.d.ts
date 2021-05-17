ed021f822f68d0ad66c8fe728d6b2affa704f2e9
Eduard Aksamitov <e@edaks.co>
2021-05-17 19:27:08 +0300

    chore(create-auth): add docs

--- create-auth/index.d.ts
+++ create-auth/index.d.ts
@@ -2,9 +2,33 @@ import { Store } from '@logux/state'
 
 import { Client } from '../client/index.js'
 
+/**
+ * Auth store. Use {@link createAuth} to create it.
+ */
 export type Auth = Store<{
+  /**
+   * Current user’s id.
+   */
   userId: string
+  /**
+   * Current state of user authentication.
+   */
   isAuthenticated: boolean
 }>
 
+/**
+ * Create {@link Auth} store.
+ *
+ * It subscribes to changes in the user’s authentication state and id.
+ *
+ * ```js
+ * import { createAuth } from '@logux/client'
+ * import { getValue } from '@logux/state'
+ *
+ * let auth = createAuth(client)
+ * let { isAuthenticated, userId } = getValue(auth)
+ * ```
+ *
+ * @param client Logux Client.
+ */
 export function createAuth(client: Client): Auth
