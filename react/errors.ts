2ca126ceb9ae6d503b783d3a60aaba80756d604a
Eduard Aksamitov <e@euaaaio.ru>
2023-09-27 18:45:39 +0300

    tests(react): add types and errors tests

---
+++ react/errors.ts
@@ -0,0 +1,34 @@
+import type { MapStore} from 'nanostores'
+import { map } from 'nanostores'
+
+import { syncMapTemplate } from '../sync-map-template/index.js'
+import { useFilter, useSync } from './index.js'
+
+type Post = {
+  id: string
+  title: string
+}
+
+let $post = syncMapTemplate<Post>('posts')
+
+let post = useSync($post, '10')
+let postList = useFilter($post, { id: '10' })
+
+let $custom = (id: string): MapStore<Post> => map({ id, text: 'A' })
+let custom = useSync($custom, '10')
+
+if (post.isLoading) {
+  // THROWS Property 'title' does not exist
+  post.title = 'New title'
+}
+
+let postListItem = postList.stores.get('10')!.value!
+if (postListItem.isLoading) {
+  // THROWS Property 'title' does not exist
+  postListItem.title = 'New title'
+}
+
+if (custom.isLoading) {
+  // THROWS Property 'title' does not exist
+  custom.title = 'B'
+}
