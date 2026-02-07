3c7b909a32ca5028976410eb56f4d5af5b527c0b
Andrey Sitnik <andrey@sitnik.ru>
2026-02-07 23:01:05 +0000

    Fix infinite loop

--- encrypt-actions/index.js
+++ encrypt-actions/index.js
@@ -155,7 +155,7 @@ export function encryptActions(client, secret, opts = {}) {
   client.node.options.onReceive = onReceive
 
   client.log.on('clean', (action, meta) => {
-    if (meta.sync && !ignore.has(action.type)) {
+    if (meta.sync && !ignore.has(action.type) && action.type !== '0/clean') {
       client.log.add({ id: meta.id, type: '0/clean' }, { sync: true })
     }
   })
